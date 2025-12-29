8b5b5b2415d59933936181e59244bb0d95f8433d
Rignu1503 <rigoberto.miranda1503@gmail.com>
2025-12-28 21:14:57 -0500

    fix(dashboard) The responsive design was organized.

--- src/app/panel/components/aside/aside.component.ts
+++ src/app/panel/components/aside/aside.component.ts
@@ -31,21 +31,14 @@ export class AsideComponent implements OnInit {
       label: 'Dashboard',
       route: '/panel/dashboard',
       icon: `
-        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24">
-          <path fill="currentColor" d="M3 13h8V3H3v10Zm0 8h8v-6H3v6Zm10 0h8V11h-8v10Zm0-18v6h8V3h-8Z"/>
-        </svg>
+
       `,
     },
     {
       label: 'Comparación por país',
       route: '/panel/by-country',
       icon: `
-        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24">
-          <g fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <rect x="3" y="3" width="7" height="18" rx="1"/>
-            <rect x="14" y="3" width="7" height="13" rx="1"/>
-          </g>
-        </svg>
+       
       `,
     },
   ];
